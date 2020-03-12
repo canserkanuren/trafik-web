64243aff0a1fd054f3b43b27ac35ddd3a990e10e
canserkanuren <canserkan.uren@gmail.com>
2020-03-12 09:32:49 +0100

    :bug: redux thunk instead of react thunk

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import thunk from 'react-thunk';
+import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
-export const store = createStore(reducers, applyMiddleware(logger));
+const middlewares = [logger, thunk];
+
+export const store = createStore(reducers, applyMiddleware(...middlewares));
